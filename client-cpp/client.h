8f09b1ffecf15795e2e5671172225c84cee97c97
najanajah <104294671+najanajah@users.noreply.github.com>
2024-04-04 23:18:06 +0800

    removing old files

--- client-cpp/client.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
-#include "utils.h"
-#include "connection.h"
-#include "service.h"
-#include "constants.h"
-
-int main(int argc, char* argv[]);
--- test.java
+++
@@ -1,8 +0,0 @@
-import java.net.*; 
-improt java.io.*; 
-public class UDPClient{ 
-    public static void main(Strings args[]){
-        DatagramSocket aSocket = null; 
-        
-    }   
-}
