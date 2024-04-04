0b567e41f5fea7e97a77e8f5950a960204093b37
zcmcxm <akamaru.zcmclc@gmail.com>
2024-04-04 09:09:29 +0800

    document code

--- server/src/main/java/com/server/helper/Util.java
+++ server/src/main/java/com/server/helper/Util.java
@@ -29,14 +29,17 @@ public class Util {
         return successPacket;
     }
 
+    // error message for unsuccessful unmarshalling
     public static String failUnMarshalMsg(byte[] data) {
         return "Fail to Unmarshal 0x" + DatatypeConverter.printHexBinary(data);
     }
 
+    // error message for invalid file path
     public static String invalidPathMsg(String path) {
         return "Invalid Path " + path;
     }
 
+    // error message for file not exist
     public static String nonExistFileMsg(String file) {
         return "File on path " + file + " does not exist";
     }
@@ -47,6 +50,7 @@ public class Util {
             byte[] data = marshal(messageType, requestId, response);
             DatagramPacket reply = new DatagramPacket(data, data.length, address, port);
 
+            // simulate the scenario where replies are lost
             if (lostReplyCount > 0) {
                 lostReplyCount--;
                 logger.info("(Lost) Reply to " + address.toString() + " at port " + port + " contents: " + response);
@@ -71,13 +75,16 @@ public class Util {
         return errorMsg;
     }
 
+    // marshal the replies
     public static byte[] marshal(char messageType, int requestId, List<Object> parameters) {
         // initial size
         ByteBuffer buffer = ByteBuffer.allocate(Constants.MAX_PACKET_SIZE);
 
+        // add the common header i.e. messageType (char) and requestId (int)
         buffer.put((byte) messageType);
         buffer.putInt(requestId);
 
+        // marshal other parameters based on their type i.e. string, int, long
         for (Object arg : parameters) {
             if (arg instanceof String) {
                 // write string length followed by string bytes
@@ -102,23 +109,26 @@ public class Util {
         return data;
     }
 
+    // unmarshal the requests
     public static List<Object> unmarshal(byte[] data) {
         // big-endian default
         ByteBuffer buffer = ByteBuffer.wrap(data);
         List<Object> request = new ArrayList<>();
 
+        // check if the common header is presented
         if (buffer.remaining() < 1 + Integer.BYTES) {
             throw new IllegalArgumentException("Insufficient data for header");
         }
 
-        // read the message type from the header and convert to char
+        // read the messageType from the header and convert to char
         char messageType = (char) buffer.get();
         request.add(messageType);
 
-        // read the request ID from the header
+        // read the requestID from the header
         int requestId = buffer.getInt();
         request.add(requestId);
 
+        // unmarshal the other parameters based on the predefined formats for each service
         try {
             switch (messageType) {
                 case Constants.REQUEST_CODE_READ: // read
@@ -147,10 +157,13 @@ public class Util {
         return request;
     }
 
+    // parse the string based on the structure of | stringLength (int) | content (unfixed length) |
+    // the first 32 bits (int) indicate the length of the string content
+    // with that length, the content can be extracted
     public static String readString(ByteBuffer buffer) {
         try {
             int filePathLen = buffer.getInt();
-            // Check for negative length or length longer than remaining buffer
+            // check for negative length or length longer than remaining buffer
             if (filePathLen < 0 || filePathLen > buffer.remaining()) {
                 throw new IllegalArgumentException("Invalid string length: " + filePathLen);
             }
@@ -158,11 +171,12 @@ public class Util {
             buffer.get(filePathBytes);
             return new String(filePathBytes, StandardCharsets.UTF_8);
         } catch (BufferUnderflowException e) {
-            // This exception is thrown if there aren't enough bytes in the buffer
+            // this exception is thrown if there aren't enough bytes in the buffer
             throw new IllegalArgumentException(Constants.INSUFFICIENT_DATA_ERROR_MSG + " for string", e);
         }
     }
 
+    // parse the int type
     private static int readInt(ByteBuffer buffer) {
         try {
             if (buffer.remaining() < Integer.BYTES) {
@@ -170,11 +184,12 @@ public class Util {
             }
             return buffer.getInt();
         } catch (BufferUnderflowException e) {
-            // This exception is thrown if there aren't enough bytes to read the data type or the number
+            // this exception is thrown if there aren't enough bytes to read the data type or the number
             throw new IllegalArgumentException(Constants.INSUFFICIENT_DATA_ERROR_MSG, e);
         }
     }
 
+    // parse the long type
     private static Long readLong(ByteBuffer buffer) {
         try {
             if (buffer.remaining() < Long.BYTES) {
@@ -182,7 +197,7 @@ public class Util {
             }
             return buffer.getLong();
         } catch (BufferUnderflowException e) {
-            // This exception is thrown if there aren't enough bytes to read the data type or the number
+            // this exception is thrown if there aren't enough bytes to read the data type or the number
             throw new IllegalArgumentException(Constants.INSUFFICIENT_DATA_ERROR_MSG, e);
         }
     }
